511354e3ffc3b317bd08da083bd8092dda7a597e
ElizabethWamboi <wamboielizabeth276@gmail.com>
2025-11-20 08:24:25 +0300

    add palindrome html file

---
+++ pali.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("input");
+
+function reversestring(str) {
+    return str.split("").reverse("").join("");
+}
+
+function check() {
+    const value = input.value;
+    const reverse = reversestring(value);
+
+    if (value===reverse){
+        alert("P A L I N D R O M E!");
+    }
+    else {
+        alert("NOT palindrome!");
+    }
+}
